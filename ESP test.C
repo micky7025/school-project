723590f279657b8c75bf5e06ab75a6606b84a773
micky7025 <62147480+micky7025@users.noreply.github.com>
2020-05-05 21:33:07 +0800

    Create ESP test.C

---
+++ ESP test.C
@@ -0,0 +1,18 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define pin 2
+
+OneWire oneWire(pin);
+DallasTemperature sensors(&oneWire); //call DataBase
+
+void setup (void)
+  {
+    Serial.begin(9600);
+    sensors.begin();
+  }
+void loop (void)
+  {
+    sensors.requestTemperatures();
+    Serial.println(sensors.getTempCByIndex(0));
+    delay(2000);
+  }
