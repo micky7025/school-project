2536519744898cef7b21235f98209e7f3df44561
micky7025 <62147480+micky7025@users.noreply.github.com>
2020-06-11 13:25:09 +0800

    Create 轉換.C

---
+++ 轉換.C
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+int input,output,i,data[20],num,n,judge;
+
+void doit(int input){
+    
+    num = output = input;
+    while(num!=0){
+        num/=16;
+        n++;
+    }
+    n*=4;
+    printf("\n二進制位元數為:%d\n",n);
+    for(i=0;i<n;i++){
+        data[n-i-1]=input%2;
+        input/=2;
+    }
+    
+    printf("十進制為%d,二進制為:",output);
+    for(i=0;i<n;i++)
+        printf("%d",data[i]);
+    printf("\n______________________________\n");
+}
+
+
+void Jud(int judge){
+    
+    if(judge==1){
+        for(i=0;i<n;i++){
+        if(data[i]==0)
+            data[i]=1;
+        else
+            data[i]=0;
+        }
+        
+        data[n]+=1;    
+        for(i=0;i<n;i++){
+            if(data[i]==2){
+                data[i]=0;
+                data[i-1]+=1;
+            }
+        }                   
+    }
+    printf("輸入值的二的補數為:");   
+    for(i=0;i<n;i++)
+        printf("%d",data[i]);
+    
+}
+
+int main(){
+        
+    printf("輸入一十六進制值");
+    scanf("%x", &input);
+    printf("輸入正負位元");
+    scanf("%x", &judge);
+    doit(input);
+    Jud(judge);
+    
+    return 0;
+}
